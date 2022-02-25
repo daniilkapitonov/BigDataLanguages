833c5fb0903c3ca6bcc7ee9410aaa8ec7bf3ca9a
daniilkapitonov <62303616+daniilkapitonov@users.noreply.github.com>
2022-02-25 17:15:55 +0300

    lab3_3 10%

---
+++ Лаб 3 9 вариант/3/src/daniil/lab/lab3_3.java
@@ -0,0 +1,14 @@
+package daniil.lab;
+
+public class lab3_3 {
+    public static class photo
+    {
+        private String ph_name;
+        private String f_pass;
+    }
+    public static class photoalbum extends photo
+    {
+        private String alb_name;
+        photo[] album;
+    }
+}
